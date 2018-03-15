5fb77b7917518e11414f559cb0b0a7f17e9cdd70
kaparray <kaparray@gmail.com>
2018-03-15 20:05:33 +0300

    Finish 9-10 practice

---
+++ Practice/Practice12/C/Practice_12/Practice_12/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  Practice_12
+//
+//  Created by Kirill on 15.03.2018.
+//  Copyright © 2018 Kirill. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+   
+    
+    
+    return 0;
+}
