16b95c1d46b85549faf643bf2f120869ebe0b02b
kaparray <kaparray@gmail.com>
2018-09-04 19:54:15 +0300

    4th September

---
+++ Course_3/Programming/Labs/Lab_1/project_1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace project_1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Cyan;    // Set backgrount color
+            Console.ForegroundColor = ConsoleColor.Black;   // Set text color
+
+
+            Console.SetWindowSize(40, 70);  // Set console size in winows 
+            Console.Title = "Project 1";    // Set title in console
+           
+            // Console.SetCursorPosition(2,4); // Set cursor position in console
+            // Console.LargestWindowWidth = 100; // Only in Windows
+
+            Console.CursorLeft = 10;
+
+            int a = 0;
+			int b = 0;
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter the a number: ");
+            a = int.Parse(Console.ReadLine());
+			Consile.WriteLine("Enter the b number: ");
+			b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Your number is " + (a+b));
+
+
+        }
+    }
+}
