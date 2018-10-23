f90790d1707d40cb050cad4dbc90a326e2934e82
kaparray <kaparray@gmail.com>
2018-10-23 21:01:41 +0300

    Finish lab_10

---
+++ Course_3/Data base/SQLQuery1.sql
@@ -0,0 +1,28 @@
+SELECT * FROM [dbo].[titles] WHERE Price < 20
+SELECT * FROM [dbo].[titles] WHERE Price > 20
+SELECT * FROM [dbo].[titles] WHERE Price = 20
+SELECT * FROM [dbo].[titles] WHERE Price  IS NULL
+
+SELECT * FROM [dbo].[titles]
+
+SELECT * FROM [dbo].[titles] WHERE Price  IS NULL
+SELECT * FROM [dbo].[titles] WHERE Price  IS NOT NULL
+
+
+SELECT Title + '(' + CAST(Price AS varchar) + ')' FROM titles
+
+SELECT 1 + 1
+SELECT 1 + '1'
+SELECT '1' + '1'
+
+SELECT ISNULL(CAST(Price AS varchar),'Lol') FROM titles
+
+
+-- Операции приведения типов CAST + CONVERT
+SELECT CAST('1' AS INT) + 1
+
+SELECT Convert(varchar, 1) + '1'
+
+
+SELECT Convert(varchar, GETDATE(), 103)
+
---
+++ Course_3/Data base/SQLQuery2.sql
@@ -0,0 +1,5 @@
+SELECT CompanyName + ' : ' +  ISNULL(Fax,'не имеет факс') FROM [dbo].[Customers]
+
+SELECT Fax FROM [dbo].[Customers] WHERE City='London' and Fax IS NOT NULL ORDER BY ContactName ASC
+-- Если написать DESC, то обратная сортировка
+-- ASC по возрастанию
