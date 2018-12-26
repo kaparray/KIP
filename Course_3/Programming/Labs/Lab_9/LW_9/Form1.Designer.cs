7b363bfec216c10143bc4f726a520bc7159ad2c3
kaparray <kaparray@gmail.com>
2018-12-27 01:30:58 +0300

    Hot fix in lab_8, lab_9 and add lab_20

--- Course_3/Programming/Labs/Lab_9/LW_9/Form1.Designer.cs
+++ Course_3/Programming/Labs/Lab_9/LW_9/Form1.Designer.cs
@@ -42,12 +42,18 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.SuspendLayout();
             // 
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox1.Location = new System.Drawing.Point(171, 54);
+            this.textBox1.Location = new System.Drawing.Point(390, 54);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
@@ -56,7 +62,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(21, 83);
+            this.button1.Location = new System.Drawing.Point(577, 85);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(126, 47);
             this.button1.TabIndex = 1;
@@ -66,7 +72,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(21, 201);
+            this.button2.Location = new System.Drawing.Point(577, 217);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(126, 47);
             this.button2.TabIndex = 3;
@@ -77,7 +83,7 @@
             // textBox2
             // 
             this.textBox2.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox2.Location = new System.Drawing.Point(171, 174);
+            this.textBox2.Location = new System.Drawing.Point(390, 174);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
             this.textBox2.ReadOnly = true;
@@ -87,7 +93,7 @@
             // button3
             // 
             this.button3.Enabled = false;
-            this.button3.Location = new System.Drawing.Point(21, 338);
+            this.button3.Location = new System.Drawing.Point(577, 354);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(126, 47);
             this.button3.TabIndex = 5;
@@ -98,7 +104,7 @@
             // textBox3
             // 
             this.textBox3.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox3.Location = new System.Drawing.Point(171, 306);
+            this.textBox3.Location = new System.Drawing.Point(390, 306);
             this.textBox3.Multiline = true;
             this.textBox3.Name = "textBox3";
             this.textBox3.ReadOnly = true;
@@ -108,33 +114,33 @@
             // textBox4
             // 
             this.textBox4.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox4.Location = new System.Drawing.Point(352, 53);
+            this.textBox4.Location = new System.Drawing.Point(12, 53);
             this.textBox4.Multiline = true;
             this.textBox4.Name = "textBox4";
             this.textBox4.ReadOnly = true;
             this.textBox4.Size = new System.Drawing.Size(362, 108);
             this.textBox4.TabIndex = 6;
-            this.textBox4.Text = "a1 = new Client();\r\na1.name = \"Джафаров Ренад Рамин оглы\";\r\na1.number = 17;\r\na1.b" +
-    "alance = 8;\r\ntextBox1.Text = \"Создан объект a1\" + Environment.NewLine + Environm" +
-    "ent.NewLine + a1;\r\n";
+            this.textBox4.Text = "a1 = new Client();\r\na1.name = \"Адещенко Кирилл\";\r\na1.number = 17;\r\na1.balance = 8" +
+    ";\r\ntextBox1.Text = \"Создан объект a1\" + Environment.NewLine + Environment.NewLin" +
+    "e + a1;\r\n";
             // 
             // textBox5
             // 
             this.textBox5.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox5.Location = new System.Drawing.Point(352, 173);
+            this.textBox5.Location = new System.Drawing.Point(12, 173);
             this.textBox5.Multiline = true;
             this.textBox5.Name = "textBox5";
             this.textBox5.ReadOnly = true;
             this.textBox5.Size = new System.Drawing.Size(362, 108);
             this.textBox5.TabIndex = 7;
-            this.textBox5.Text = "a2 = a1;\r\na2.name = \"Джафаров Давид Раминович\";\r\na2.number = 23;\r\na2.balance = 5;" +
-    "\r\ntextBox2.Text = \"Инициализирован объект a2 объектом a1\" + Environment.NewLine " +
-    "+ Environment.NewLine + a2;";
+            this.textBox5.Text = "a2 = a1;\r\na2.name = \"Киреева Галина\";\r\na2.number = 23;\r\na2.balance = 5;\r\ntextBox2" +
+    ".Text = \"Инициализирован объект a2 объектом a1\" + Environment.NewLine + Environm" +
+    "ent.NewLine + a2;";
             // 
             // textBox6
             // 
             this.textBox6.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox6.Location = new System.Drawing.Point(352, 306);
+            this.textBox6.Location = new System.Drawing.Point(12, 306);
             this.textBox6.Multiline = true;
             this.textBox6.Name = "textBox6";
             this.textBox6.ReadOnly = true;
@@ -145,7 +151,7 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(597, 417);
+            this.button4.Location = new System.Drawing.Point(715, 445);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(106, 54);
             this.button4.TabIndex = 9;
@@ -157,7 +163,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(37, 20);
+            this.label1.Location = new System.Drawing.Point(601, 14);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(90, 21);
@@ -168,30 +174,57 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(213, 20);
+            this.label2.Location = new System.Drawing.Point(425, 14);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(86, 21);
+            this.label2.Size = new System.Drawing.Size(99, 21);
             this.label2.TabIndex = 11;
-            this.label2.Text = "Результат";
+            this.label2.Text = "Результаты";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label3.Location = new System.Drawing.Point(511, 20);
+            this.label3.Location = new System.Drawing.Point(167, 9);
             this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(41, 21);
+            this.label3.Size = new System.Drawing.Size(54, 21);
             this.label3.TabIndex = 12;
-            this.label3.Text = "Код";
+            this.label3.Text = "Коды";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Beige;
+            this.pictureBox1.Location = new System.Drawing.Point(-3, 38);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(839, 130);
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.PaleGreen;
+            this.pictureBox2.Location = new System.Drawing.Point(-18, 167);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(862, 133);
+            this.pictureBox2.TabIndex = 14;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Thistle;
+            this.pictureBox3.Location = new System.Drawing.Point(-3, 299);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(839, 140);
+            this.pictureBox3.TabIndex = 15;
+            this.pictureBox3.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(715, 483);
+            this.ClientSize = new System.Drawing.Size(834, 511);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -205,11 +238,19 @@
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox3);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximumSize = new System.Drawing.Size(850, 550);
+            this.MinimumSize = new System.Drawing.Size(850, 550);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Лабораторная работа №9";
+            this.Text = "Лабораторная работа №9 Адещенко Кирилл";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -230,6 +271,9 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
     }
 }
 
--- Course_3/Programming/Labs/Lab_9/LW_9/Form1.cs
+++ Course_3/Programming/Labs/Lab_9/LW_9/Form1.cs
@@ -29,9 +29,9 @@ namespace LW_9
         {
             
             a1 = new Client();
-            a1.name = "Джафаров Ренад Рамин оглы";
-            a1.number = 17;
-            a1.balance = 8;
+            a1.name = "Адещенко Кирилл";
+            a1.number = 19;
+            a1.balance = 432;
             textBox1.Text = "Создан объект a1" + Environment.NewLine + Environment.NewLine + a1;
             button2.Enabled = true;
         }
@@ -44,16 +44,15 @@ namespace LW_9
         private void button2_Click(object sender, EventArgs e)
         {
             a2 = a1;
-            a2.name = "Джафаров Давид Раминович";
-            a2.number = 23;
-            a2.balance = 5;
+            a2.name = "Киреева Галина";
+            a2.number = 44;
+            a2.balance = 3;
             textBox2.Text = "Инициализирован объект a2 объектом a1" + Environment.NewLine + Environment.NewLine + a2;
             button3.Enabled = true;
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-
             a3 = a1;
             textBox3.Text = "Инициализирован объект a3 объектом a1" + Environment.NewLine + Environment.NewLine + a3;
         }
