5e37fef1b3d4d9680ad5709e85362041fb929d89
kaparray <kaparray@gmail.com>
2018-12-27 01:30:07 +0300

    Hot fix in lab_2, lab_3, lab_4, lab_7

---
+++ Course_3/Programming/Labs/Lab_2/Function/fun.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Func
+{
+    class fun
+    {
+        int b = 0;
+
+        public double func(int x, double a)
+        {
+            return pfunc(x, a);
+        }
+
+        double pfunc(int x, double a)
+        {
+            double y = 0;
+            if (x < 0)
+            { 
+                
+                try
+                {
+                    y = x / Math.Log(x + a);
+                }
+                catch (ArithmeticException e)
+                {
+                    Console.WriteLine("Devide by zero");
+                }
+            }  
+            else if (x >= 0)
+            {
+                y = Math.Pow(x, 2) * Math.Cos(x + a);
+            }
+            return y;
+        }
+    }
+}
